966bba8fedbfd341c82b3e754bfb94f3ecb9828d
ignacio.munoz2001@alumnos.ubiobio.cl <ignacio.munoz2001@alumnos.ubiobio.cl>
2023-06-06 18:08:14 -0400

    Co-authored-by: SrTimin <SrTimin@users.noreply.github.com> Co-authored-by: Alexander <alezcf@users.noreply.github.com>

--- routes/AttendanceRouter.js
+++ routes/AttendanceRouter.js
@@ -4,5 +4,6 @@ const attendanceController = require('../controllers/AttendanceController');
 
 router.post('/create', attendanceController.createAttendance);
 router.get('/read', attendanceController.getAllAttendance);
+router.get('/readDaily', attendanceController.getDailyAttendance);
 
 module.exports = router;
