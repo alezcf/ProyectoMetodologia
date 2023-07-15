b213eb2ea13a7ae5a4f13af4e3f13d312226ab50
Fig-Joaquin <joaquin.sanchez2101@alumnos.ubiobio.cl>
2023-07-15 19:08:34 -0400

    Resolucion problema bd

--- Frontend/src/screens/HomeScreen.jsx
+++ Frontend/src/screens/HomeScreen.jsx
@@ -2,89 +2,53 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const HomeScreen = () => {
-    const mensajeError = null; // Aquí puedes asignar un mensaje de error si lo deseass...
-    const [rut, setRut] = useState("");
-    const [dv, setDv] = useState("");
-    const [password, setPassword] = useState("");
+    const [mensajeError, setMensajeError] = useState(Error); // Aquí puedes asignar un mensaje de error si lo deseas.
+    const [rut, setRut] = useState('');
+    const [dv, setDv] = useState('');
+    const [password, setPassword] = useState('');
 
     const rutChange = (e) => {
         setRut(e.target.value);
-    }   
+    };
+    
     const dvChange = (e) => {
         setDv(e.target.value);
-    }   
+    };
+    
     const passwordChange = (e) => {
         setPassword(e.target.value);
-    }   
-
-    const handleSubmit = async () => {
-        const credentials ={
-            rut: rut,
-            password : password,
-            dv: dv
-        } 
-        console.log(credentials)
-        await axios.post("http://localhost:3001/usuario/login", credentials)
-        .then(res => {
-            console.log(res.data);
-            window.location.href = "/";
-    }) 
-        .catch(err =>{
-        console.log(err);
-
-        }  )
     };
 
+    const handleSubmit = async () => {
+        const credentials = {
+            rut: rut,
+            password: password,
+            dv: dv
+        };
 
+        console.log(credentials);
 
-    // const styles = {
-    //     background: {
-    //         backgroundColor: "#fff",
-    //     },
-    //     button: {
-    //         backgroundColor: "#d9ba52",
-    //         borderColor: "#000",
-    //     },
-    // };
-
-    
+        try {
+            const res = await axios.post('http://localhost:3001/usuario/login', credentials);
+            console.log(res.data);
+            window.location.href = '/';
+        } catch (err) {
+            console.log(err);
+            setMensajeError('Error en la solicitud');
+        }
+    };
 
     return (
-        <html>
-        <head>
-            <title>Página de Inicio de Sesión</title>
-            <link rel="stylesheet" type="text/css" href="/css/login.css" />
-            <script src="/js/functions.js" defer></script>
-            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-            </head>
-            <body>
-            <div className="container">
-                <div className="inner-container">
+        <div className="container">
+            <div className="inner-container">
                 <h1>Página de Inicio de Sesión</h1>
-                {mensajeError && (
-                    <div className="error-message">
-                    {mensajeError}
-                    </div>
-                )}
-                {/* <form id="login-form" action="/usuario/logIn" method="POST">
-                    <label htmlFor="rut">RUT:</label>
-                    <input type="text" id="rut" name="rut" placeholder='123456789' maxLength="8" required />
-                    <span>-</span>
-                    <input type="text" id="dv" name="dv" placeholder='K' maxLength="1" required />
-                    <br/><br/>
-                    <label htmlFor="password">Contraseña:</label>
-                    <input type="password" id="password" name="password" maxLength="20" required />
-                    <br/><br/>
-                    <input type="submit" style={styles.button} value="Ingresar" />
-                </form> */}
-                <input value = {rut} onChange = {rutChange}></input>
-                <input value = {dv} onChange = {dvChange}></input>
-                <input value = {password} onChange = {passwordChange}></input>
-                <button onClick={handleSubmit}>Ingresar</button> 
-                </div>
+                {mensajeError && <div className="error-message">{mensajeError}</div>}
+                <input value={rut} onChange={rutChange}></input>
+                <input value={dv} onChange={dvChange}></input>
+                <input value={password} onChange={passwordChange}></input>
+                <button onClick={handleSubmit}>Ingresar</button>
             </div>
-            </body>
-        </html>
+        </div>
     );
 };
 
