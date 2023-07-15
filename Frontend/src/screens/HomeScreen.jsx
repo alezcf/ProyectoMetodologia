ae0d5d14d64a818e070e142b9e96a2db5cf9c315
Fig-Joaquin <joaquin.sanchez2101@alumnos.ubiobio.cl>
2023-07-15 18:58:05 -0400

    creacion de la conexion del backend y frontend

--- Frontend/src/screens/HomeScreen.jsx
+++ Frontend/src/screens/HomeScreen.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const HomeScreen = () => {
-    const mensajeError = null; // Aquí puedes asignar un mensaje de error si lo prefieres.
+    const mensajeError = null; // Aquí puedes asignar un mensaje de error si lo deseass...
     const [rut, setRut] = useState("");
     const [dv, setDv] = useState("");
     const [password, setPassword] = useState("");
@@ -28,11 +28,11 @@ const HomeScreen = () => {
         .then(res => {
             console.log(res.data);
             window.location.href = "/";
-          }) 
-          .catch(err =>{
-          console.log(err);
+    }) 
+        .catch(err =>{
+        console.log(err);
 
-          }  )
+        }  )
     };
 
 
@@ -50,31 +50,41 @@ const HomeScreen = () => {
     
 
     return (
-        <div className="container">
-            <div className="inner-container">
-            <h1>Página de Inicio de Sesión</h1>
-            {mensajeError && (
-                <div className="error-message">
-                {mensajeError}
+        <html>
+        <head>
+            <title>Página de Inicio de Sesión</title>
+            <link rel="stylesheet" type="text/css" href="/css/login.css" />
+            <script src="/js/functions.js" defer></script>
+            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+            </head>
+            <body>
+            <div className="container">
+                <div className="inner-container">
+                <h1>Página de Inicio de Sesión</h1>
+                {mensajeError && (
+                    <div className="error-message">
+                    {mensajeError}
+                    </div>
+                )}
+                {/* <form id="login-form" action="/usuario/logIn" method="POST">
+                    <label htmlFor="rut">RUT:</label>
+                    <input type="text" id="rut" name="rut" placeholder='123456789' maxLength="8" required />
+                    <span>-</span>
+                    <input type="text" id="dv" name="dv" placeholder='K' maxLength="1" required />
+                    <br/><br/>
+                    <label htmlFor="password">Contraseña:</label>
+                    <input type="password" id="password" name="password" maxLength="20" required />
+                    <br/><br/>
+                    <input type="submit" style={styles.button} value="Ingresar" />
+                </form> */}
+                <input value = {rut} onChange = {rutChange}></input>
+                <input value = {dv} onChange = {dvChange}></input>
+                <input value = {password} onChange = {passwordChange}></input>
+                <button onClick={handleSubmit}>Ingresar</button> 
                 </div>
-            )}
-            {/* <form id="login-form" action="/usuario/logIn" method="POST">
-                <label htmlFor="rut">RUT:</label>
-                <input type="text" id="rut" name="rut" placeholder='123456789' maxLength="8" required />
-                <span>-</span>
-                <input type="text" id="dv" name="dv" placeholder='K' maxLength="1" required />
-                <br/><br/>
-                <label htmlFor="password">Contraseña:</label>
-                <input type="password" id="password" name="password" maxLength="20" required />
-                <br/><br/>
-                <input type="submit" style={styles.button} value="Ingresar" />
-            </form> */}
-            <input value = {rut} onChange = {rutChange} />
-            <input value = {dv} onChange = {dvChange} />
-            <input value = {password} onChange = {passwordChange} />
-            <button onClick={handleSubmit}>Ingresar</button> 
             </div>
-        </div>
+            </body>
+        </html>
     );
 };
 
