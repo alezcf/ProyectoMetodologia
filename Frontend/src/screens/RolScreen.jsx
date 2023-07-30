69fd5437d7b169043f448f2d1e3112d071c132d5
Fig-Joaquin <joaquin.sanchez2101@alumnos.ubiobio.cl>
2023-07-30 05:42:11 -0400

    Se añadio expirationRol en rolController, se crea la rolScreen

---
+++ Frontend/src/screens/RolScreen.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const RolScreen = () => {
+  return (
+    <>
+      <main className="container"></main>   
+       <div>
+            1
+       </div>
+       <div>
+            2
+        </div>
+    </>
+  )
+}
+
+export default RolScreen
