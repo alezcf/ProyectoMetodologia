09df60a370a15e283c9ff2f13c50be5a2a11430e
Alexander <ialeczander@gmail.com>
2023-07-31 01:12:25 -0400

    fix buttons for AttendanceScreen

--- Frontend/src/screens/AttendanceScreen.jsx
+++ Frontend/src/screens/AttendanceScreen.jsx
@@ -3,7 +3,15 @@ import axios from 'axios';
 import { useHistory, useLocation } from 'react-router-dom';
 import '../css/AttendanceScreen.css';
 import moment from 'moment';
-import { FaEdit, FaTrash, FaArrowLeft, FaDownload } from 'react-icons/fa';
+import Button from '@mui/material/Button';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks'; // Importar el icono LibraryBooks
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
+import { makeStyles } from '@mui/styles';
 
 const AttendanceScreen = () => {
   
@@ -98,6 +106,7 @@ const AttendanceScreen = () => {
       const res = await axios.get(downloadUrl, {
         responseType: 'blob',
       });
+
 
       const url = window.URL.createObjectURL(new Blob([res.data]));
       const link = document.createElement('a');
@@ -200,6 +209,62 @@ const AttendanceScreen = () => {
     }
   };
 
+  const buttonStyle = {
+    backgroundColor: '#3A789E',  // Set the background color to blue
+    color: 'white',           // Set the text color to white
+    padding: '5px 8px',     // Adjust the padding to make the button smaller
+    fontSize: '14px',
+    font: 'Arial'         // Adjust the font size to make the text smaller
+  };
+
+  const buttonStyleMenu = {
+    backgroundColor: '#3A789E',  // Set the background color to blue
+    color: 'white',           // Set the text color to white
+    padding: '8px 15px',     // Adjust the padding to make the button smaller
+    fontSize: '14px',
+    font: 'Arial'         // Adjust the font size to make the text smaller
+  };
+
+  const buttonStyleDelete = {
+    backgroundColor: '#C90040',  // Set the background color to red
+    color: 'white',          // Set the text color to white
+    padding: '5px 8px',     // Adjust the padding to make the button smaller
+    fontSize: '14px',
+    font: 'Arial'        // Set the text color to white
+  };
+
+  const buttonStyleGoBack = {
+    backgroundColor: '#3A789E',  // Set the background color to blue
+    color: 'white',           // Set the text color to white
+    padding: '10px 20px',       // Adjust the padding to make the button smaller
+    fontSize: '14px',         // Adjust the font size to make the text smaller
+    fontFamily: 'Arial',      // Set the font family
+  };
+
+  const buttonStyleDownload = {
+    backgroundColor: '#00AD45',
+    color: 'white',
+    padding: '10px 20px',
+    fontSize: '14px',
+    fontFamily: 'Arial',
+    position: 'absolute', // Set the position to absolute
+    top: '60px',          // Set the top position to create a margin of 60px from the top
+    right: '20px',        // Set the right position to move the button to the right
+  };
+
+
+
+  const buttonContainerStyle = {
+    display: 'flex',
+    gap: '10px', // Add a gap of 10px between the buttons
+  };
+
+  const backButtonContainerStyle = {
+    position: 'relative',     // Set the container position to relative
+    top: '-10px',              // Set the top position to create a margin of 30px from the top
+    right: '700px',                // Set the left position to align the button to the left
+  };
+  
   const renderAttendanceRows = () => {
     const attendanceData = searchRut.trim() === '' ? arrayAttendance : filteredAttendance;
 
@@ -210,12 +275,24 @@ const AttendanceScreen = () => {
         {user?.jobTitle === 'Jefe de Brigada' && (
           <td className="centered-cell">
             <div className="actions-container">
-              <button className="button-edit" onClick={() => handleDateModification(attendance)}>
-                <FaEdit /> Modificar
-              </button>
-              <button className="button-delete" onClick={() => handleDeleteAttendance(attendance)}>
-                <FaTrash /> Eliminar
-              </button>
+            <div style={buttonContainerStyle}>
+        <Button
+          variant="contained"
+          style={buttonStyle}
+          startIcon={<EditIcon />}
+          onClick={() => handleDateModification(attendance)}
+        >
+          Modificar
+        </Button>
+        <Button
+          variant="contained"
+          style={buttonStyleDelete}
+          startIcon={<DeleteIcon />}
+          onClick={() => handleDeleteAttendance(attendance)}
+        >
+          Eliminar
+        </Button>
+      </div>
             </div>
           </td>
         )}
@@ -223,82 +300,130 @@ const AttendanceScreen = () => {
     ));
   };
 
+  const useStyles = makeStyles((theme) => ({
+    searchContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+      margin: '10px 0',
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)', // Agrega la sombra al campo de búsqueda
+    },
+    searchInput: {
+      width: 250, // Ajusta el ancho a tu preferencia
+    }, 
+    searchIcon: {
+      color: '#6A7A7A', // Cambia el color del icono a rojo
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
     <div>
-        <div className="user-bar">
-          <p className="user-name">Bienvenido {user?.names + " " + user?.lastName}</p>
-        </div>
-    <div className="attendance-container">
 
-      <h1 className="attendance-title">Listado de Asistencias</h1>
-      {errorMessage && <div className="error-message">{errorMessage}</div>}
-      <div className="options-menu">
-        <button
-          className={`black-button ${selectedOption === 'Todos' ? 'selected' : ''}`}
-          onClick={() => handleOptionChange('Todos')}
-          title="Todos los registros"
-        >
-          <FaEdit />
-          Todos
-        </button>
-        <button
-          className={`black-button ${selectedOption === 'Diaria' ? 'selected' : ''}`}
-          onClick={() => handleOptionChange('Diaria')}
-          title="Registros diarios"
-        >
-          <FaEdit />
-          Diaria
-        </button>
-        <button
-          className={`black-button ${selectedOption === 'Semanal' ? 'selected' : ''}`}
-          onClick={() => handleOptionChange('Semanal')}
-          title="Registros semanales"
-        >
-          <FaEdit />
-          Semanal
-        </button>
-        <button
-          className={`black-button ${selectedOption === 'Mensual' ? 'selected' : ''}`}
-          onClick={() => handleOptionChange('Mensual')}
-          title="Registros mensuales"
-        >
-          <FaEdit />
-          Mensual
-        </button>
+      
+      <div className="user-bar">
+        <p className="user-name">{user?.names + ' ' + user?.lastName}</p>
       </div>
-      <div className="search-container">
-        <div className="search-wrapper">
-          <input
+
+
+      
+      
+      <div className="attendance-container">
+        <div style={backButtonContainerStyle}>
+          <Button
+            variant="contained"
+            style={buttonStyleGoBack}
+            startIcon={<ArrowBackIcon />}
+            onClick={handleGoBack}
+          >
+            Volver
+          </Button>
+        </div>
+        <h1 className="attendance-title">Listado de Asistencias</h1>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+        <div className="options-menu">
+        <Button
+            variant="contained"
+            color="secondary"
+            className={`black-button ${selectedOption === 'Todos' ? 'selected' : ''}`}
+            style={buttonStyleMenu}
+            onClick={() => handleOptionChange('Todos')}
+            title="Todos los registros"
+          >
+            <LibraryBooksIcon />
+            Todos
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            className={`black-button ${selectedOption === 'Diaria' ? 'selected' : ''}`}
+            style={buttonStyleMenu}
+            onClick={() => handleOptionChange('Diaria')}
+            title="Registros diarios"
+          >
+            <LibraryBooksIcon />
+            Diaria
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            className={`black-button ${selectedOption === 'Semanal' ? 'selected' : ''}`}
+            style={buttonStyleMenu}
+            onClick={() => handleOptionChange('Semanal')}
+            title="Registros semanales"
+          >
+            <LibraryBooksIcon /> 
+            Semanal
+          </Button>
+          <Button
+            variant="outlined"
+            className={`black-button ${selectedOption === 'Mensual' ? 'selected' : ''}`}
+            style={buttonStyleMenu}
+            onClick={() => handleOptionChange('Mensual')}
+            title="Registros mensuales"
+          >
+            <LibraryBooksIcon /> 
+            Mensual
+          </Button>
+        </div>
+</div>
+        <div className="search-container">
+        <TextField
             type="text"
-            placeholder="Buscar por RUT..."
+            label="Búsqueda por RUT..."
+            placeholder="Ejemplo: 12.345.678-9"
             value={searchRut}
             onChange={handleSearchChange}
-            className="search-input"
+            InputProps={{
+              startAdornment: <SearchIcon className={classes.searchIcon} />, // Aplica los estilos al icono
+              className: classes.searchInput, // Aplica los estilos al campo de entrada
+            }}
+            variant="outlined"
           />
         </div>
+        <table className="attendance-table">
+          <thead>
+            <tr>
+              <th>RUT Usuario</th>
+              <th>Fecha</th>
+              {user?.jobTitle === 'Jefe de Brigada' && <th>Acciones</th>}
+            </tr>
+          </thead>
+          <tbody>{renderAttendanceRows()}</tbody>
+        </table>
+
+        {user?.jobTitle === 'Jefe de Brigada' && (
+          <Button
+            variant="contained"
+            style={buttonStyleDownload}
+            startIcon={<CloudDownloadIcon />}
+            onClick={handleDownloadAttendance}
+          >
+            Descargar asistencia
+          </Button>
+        )}
       </div>
-      <table className="attendance-table">
-        <thead>
-          <tr>
-            <th>RUT Usuario</th>
-            <th>Fecha</th>
-            {user?.jobTitle === 'Jefe de Brigada' && <th>Acciones</th>}
-          </tr>
-        </thead>
-        <tbody>
-          {renderAttendanceRows()}
-        </tbody>
-      </table>
-      <button className="go-back-button" onClick={handleGoBack}>
-        <FaArrowLeft /> Volver
-      </button>
-      {user?.jobTitle === 'Jefe de Brigada' && (
-        <button className="download-button" onClick={handleDownloadAttendance}>
-          <FaDownload /> Descargar asistencia
-        </button>
-      )}
-    </div>
-    </div>
+
   );
 };
 
