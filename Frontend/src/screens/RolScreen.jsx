16bbf10d9adaab16a691157f626bb07264ee7d4e
Fig-Joaquin <joaquin.sanchez2101@alumnos.ubiobio.cl>
2023-07-31 22:52:24 -0400

    Se actualiza UpdateRolScreen con material UI

--- Frontend/src/screens/RolScreen.jsx
+++ Frontend/src/screens/RolScreen.jsx
@@ -1,12 +1,73 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import '../css/RolScreen.css'; // Importa el archivo de estilos CSS
+import '../css/RolScreen.css'; // Import the CSS styles
 import { useHistory } from 'react-router-dom';
+import { Button, Box, Grid, Table, TableHead, TableRow, TableCell, TableBody, Container, Typography } from '@mui/material'; // Import Material-UI components
+import { makeStyles } from '@mui/styles'
 
-const RolScreen = () => { 
-  const [employees, setEmployees] = useState([]); 
+const useStyles = makeStyles((theme) => ({
+  formButton: {
+    padding: theme.spacing(1, 2),
+    backgroundColor: '#405fe9',
+    color: 'white',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',
+    transition: 'background-color 0.3s ease-in-out',
+    '&:hover': {
+      backgroundColor: '#264bf2',
+    },
+  },
+  formButton2: {
+    padding: theme.spacing(1, 2),
+    backgroundColor: '#405fe9',
+    color: 'white',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',
+    transition: 'background-color 0.3s ease-in-out',
+    '&:hover': {
+      backgroundColor: '#264bf2',
+    },
+  },
+  formButton3: {
+    padding: theme.spacing(1, 2),
+    backgroundColor: '#405fe9',
+    color: 'white',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',
+    transition: 'background-color 0.3s ease-in-out',
+    '&:hover': {
+      backgroundColor: '#264bf2',
+    },
+  },
+  customButtonContainer: {
+    position: 'fixed', // Asegura que el contenedor tenga una posición para posicionar el botón dentro de él
+    top: '10px',
+    left: '20px', // Cambia el valor según la posición que desees (puedes usar top, bottom, left, right)
+  },
+  customButtonContainer2: {
+    position: 'fixed', // Asegura que el contenedor tenga una posición para posicionar el botón dentro de él
+    top: '10px',
+    left: '150px', // Cambia el valor según la posición que desees (puedes usar top, bottom, left, right)
+  },
+  customButtonContainer3: {
+    position: 'fixed', // Asegura que el contenedor tenga una posición para posicionar el botón dentro de él
+    top: '10px',
+    left: '360px', // Cambia el valor según la posición que desees (puedes usar top, bottom, left, right)
+  },
+ 
+
+}));
+
+const RolScreen = () => {
+  const [employees, setEmployees] = useState([]);
   const history = useHistory();
-
+  const classes = useStyles();
   
   useEffect(() => {
     fetchEmployees();
@@ -20,60 +81,68 @@ const RolScreen = () => {
       console.error('Error al obtener los trabajadores:', error);
     }
   };
+
   const handleButtonClick = () => {
     history.push('/actualizando');
   };
+
   const handleButtonClickAdd = () => {
     history.push('/registrando');
   };
+
   const handleGoBack = () => {
     history.goBack();
-  };  
+  };
 
   return (
+    <Container>
+      <Grid container justify="center" alignItems="center" className={classes.customButtonContainer}>
+        <Button className={classes.formButton} onClick={handleGoBack}>Atrás</Button>
+      </Grid>
+        <div className="button-container"></div>
 
-    <div> <button className='back-button' onClick={handleGoBack}>Atrás</button> 
-    <div className="button-container">
-    <button className='buttonUp' onClick={handleButtonClick}>Actualizar un cargo</button>
-    <button className='buttonUp2' onClick={handleButtonClickAdd}>Registrar Trabajador</button>
-  
-       <div  className="space">
-        <span>
-          <h1>Control de roles en la Brigada</h1>
-          <div style={{  marginLeft: '55px'}}><h4>Lista de Bomberos registrados</h4> </div>
-          
-           </span>
-       </div> 
-       
-      <div className = 'container'>
-          <div className="employee-table-container">
-            
-            <table className="employee-table">
-                <thead>
-                  <tr>
-                    <th>Rut</th>
-                    <th>Nombre</th>
-                    <th>Cargo</th>
-                    <th>Posición</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {employees.map((employee) => (
-                    <tr key={employee._id}>
-                      <td>{employee.rut}</td>
-                      <td>{`${employee.names} ${employee.lastName} ${employee.secondLastName}`}</td>
-                      <td>{employee.jobTitle}</td>
-                      <td>{employee.position}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-          </div>
-        <div>
+      <div>
+      <Grid container justify="center" alignItems="center" className={classes.customButtonContainer2}>
+        <Button className={classes.formButton2} onClick={handleButtonClick}>Actualizar un cargo</Button>
+      </Grid>
+
+      <Grid  container justify="center" alignItems="center" className={classes.customButtonContainer3}>
+        <Button className={classes.formButton3} onClick={handleButtonClickAdd}>Registrar Trabajador</Button>
+      </Grid>
+      </div>
+
+      <Box mt={10} ml={32}> {/* Apply margin-top and margin-left */}
+        <Typography variant="h3">Control de roles en la Brigada</Typography>
+        <Box mt={5} ml={20}>
+          <Typography variant="h5">Lista de Bomberos registrados</Typography>
+        </Box>
+      </Box>
+
+      <div className='container'>
+        <div className="employee-table-container">
+          <Table className="employee-table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Rut</TableCell>
+                <TableCell>Nombre</TableCell>
+                <TableCell>Cargo</TableCell>
+                <TableCell>Posición</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {employees.map((employee) => (
+                <TableRow key={employee._id}>
+                  <TableCell>{employee.rut}</TableCell>
+                  <TableCell>{`${employee.names} ${employee.lastName} ${employee.secondLastName}`}</TableCell>
+                  <TableCell>{employee.jobTitle}</TableCell>
+                  <TableCell>{employee.position}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </div>
       </div>
-    </div>
-    </div>
+    </Container>
   );
 };
 
